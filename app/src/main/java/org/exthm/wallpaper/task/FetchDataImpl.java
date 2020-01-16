ba2c10ee1e646e3bd2be70e8593616c669c45d18
googlechinaceo <dingwenxuan4@gmail.com>
2020-01-16 21:56:44 +0800

    Add more wallpapers

---
+++ app/src/main/java/org/exthm/wallpaper/task/FetchDataImpl.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.exthm.wallpaper.task;
+
+import android.app.WallpaperManager;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+
+import androidx.annotation.NonNull;
+
+import org.exthm.wallpaper.R;
+import org.exthm.wallpaper.bundle.WallpaperBundle;
+import org.exthm.wallpaper.factory.BuiltInWallpaperFactory;
+import org.exthm.wallpaper.factory.GradientWallpaperFactory;
+import org.exthm.wallpaper.factory.MonoWallpaperFactory;
+import org.exthm.wallpaper.factory.UserWallpaperFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+final class FetchDataImpl {
+    private final List<WallpaperBundle> mData = new ArrayList<>();
+    private final Callback mCallbacks;
+
+    FetchDataImpl(@NonNull final Callback callbacks) {
+        mCallbacks = callbacks;
+    }
+
+    @NonNull
+    List<WallpaperBundle> fetchData() {
+        addUser();
+        addBuiltIn();
+        addColors();
+        addGradients();
+
+        return mData;
+    }
+
+    private void addUser() {
+        mData.add(UserWallpaperFactory.build(mCallbacks.getResources()));
+    }
+
+    private void addBuiltIn() {
+        Resources res = mCallbacks.getResources();
+
+        // System wallpaper first
+        WallpaperManager manager = mCallbacks.getWallpaperManager();
+        mData.add(BuiltInWallpaperFactory.buildDefault(res, manager));
+
+        // Other built-in
+        String[] names = res.getStringArray(R.array.wallpaper_built_in_names);
+        TypedArray drawables = res.obtainTypedArray(R.array.wallpaper_built_in_drawables);
+        for (int i = 0; i < drawables.length(); i++) {
+            final int resourceId = drawables.getResourceId(i, 0);
+            if (resourceId != 0) {
+                mData.add(BuiltInWallpaperFactory.build(names[i], res, resourceId));
+            }
+        }
+
+        drawables.recycle();
+    }
+
+    private void addColors() {
+        Resources res = mCallbacks.getResources();
+        String[] names = res.getStringArray(R.array.wallpaper_mono_names);
+        TypedArray colors = res.obtainTypedArray(R.array.wallpaper_mono_colors);
+        for (int i = 0; i < colors.length(); i++) {
+            final int color = colors.getColor(i, Color.BLACK);
+            mData.add(MonoWallpaperFactory.build(names[i], color));
+        }
+
+        colors.recycle();
+    }
+
+    private void addGradients() {
+        Resources res = mCallbacks.getResources();
+        String[] names = res.getStringArray(R.array.wallpaper_gradient_names);
+        TypedArray gradients = res.obtainTypedArray(R.array.wallpaper_gradient_drawables);
+        for (int i = 0; i < gradients.length(); i++) {
+            final int resourceId = gradients.getResourceId(i, 0);
+            if (resourceId != 0) {
+                mData.add(GradientWallpaperFactory.build(names[i], res, resourceId));
+            }
+        }
+
+        gradients.recycle();
+    }
+
+    public interface Callback {
+        @NonNull
+        Resources getResources();
+
+        @NonNull
+        WallpaperManager getWallpaperManager();
+    }
+}
